1ae4a947ceabd53dcd1c0ca7b28091169b83fe3b
interind <interind@yandex.ru>
2020-07-28 11:05:53 +0300

    alert

--- script/script.js
+++ script/script.js
@@ -1,34 +1,49 @@
-let nameInput = document.querySelector('.popup__input-name');//ввод имяни
-let jobInput = document.querySelector('.popup__input-job');//ввод кем работает
-let popup = document.querySelector('.popup');//блок с формой
+let popup = document.querySelector('.popup');
+console.log(popup);
 let popupForm = document.querySelector('.popup__container');//форма
+let profileSection = document.querySelector('.profile');
+let profileInfo = profileSection.querySelector('.profile-info');
 let buttonOpenPopup = document.querySelector('.edit-button');//кнопка открытия формы
 let buttonClosePopup = document.querySelector('.popup__button-close');//кнопка закрытия формы
-let buttonSubmit = document.querySelector('.popup__button-submit');//кнопка отправки данных
+let buttonSubmit = popupForm.querySelector('.popup__button-submit');//кнопка отправки данных
+let profileTitle = document.querySelector('.profile__title');
+let nameInput = popupForm.querySelector('.popup__input-name');
+let jobInput = popupForm.querySelector('.popup__input-job');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
-function userText() {
-  nameInput = document.querySelector('.popup__input-name').value;
-  document.querySelector('.profile__title').textContent = nameInput;
-  jobInput = document.querySelector('.popup__input-job').value;
-  document.querySelector('.profile__subtitle').textContent = jobInput;
-  popup.classList.remove('popup_opened');
+function formSubmitHandler(evt) {
+		if (nameInput.value != "" && jobInput.value != ""){
+			nameInput.value;
+   profileTitle.textContent = nameInput.value;
+   jobInput.value;
+			profileSubtitle.textContent = jobInput.value;
+			popup.classList.remove('popup_opened');
+		}
+		else {
+			alert('Заполните все поля');
+		}
+		;
+		evt.preventDefault();
 }
 
-buttonOpenPopup.addEventListener('click' , function openPopup() {
-  popup.classList.add('popup_opened');
-});
-
-buttonClosePopup.addEventListener('click' , function closePopup() {
-  popup.classList.remove('popup_opened');
-});
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
+function openPopup() {
+	popup.classList.add('popup_opened');
+	nameInput.focus();
 }
+
+function closePopup() {
+	popup.classList.remove('popup_opened');
+}
+
+buttonOpenPopup.addEventListener('click' , openPopup);
+
+buttonClosePopup.addEventListener('click' , closePopup);
 
 popupForm.addEventListener('submit', formSubmitHandler);
 
-buttonSubmit.addEventListener('click', userText);
+buttonSubmit.addEventListener('click', formSubmitHandler);
+
+
 
 /*// Находим форму в DOM
 let formElement = // Воспользуйтесь методом querySelector()
