6e8cb3bf556740d84f0a20e410a43026da3081e0
interind <interind@yandex.ru>
2020-07-28 14:16:33 +0300

    inter

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
 let popup = document.querySelector('.popup');
-console.log(popup);
 let popupForm = document.querySelector('.popup__container');//форма
 let profileSection = document.querySelector('.profile');
 let profileInfo = profileSection.querySelector('.profile-info');
@@ -17,13 +16,14 @@ function formSubmitHandler(evt) {
    profileTitle.textContent = nameInput.value;
    jobInput.value;
 			profileSubtitle.textContent = jobInput.value;
-			popup.classList.remove('popup_opened');
+		}
+		else if(nameInput.value != "" || jobInput.value != ""){
+			alert('Для сохрание нужно заполнить все ваши данные');
 		}
 		else {
-			alert('Заполните все поля');
-		}
-		;
+		};
 		evt.preventDefault();
+		popup.classList.remove('popup_opened');
 }
 
 function openPopup() {
@@ -42,30 +42,3 @@ buttonClosePopup.addEventListener('click' , closePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
 
 buttonSubmit.addEventListener('click', formSubmitHandler);
-
-
-
-/*// Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-												// Так мы можем определить свою логику отправки.
-												// О том, как это делать, расскажем позже.
-
-	// Находим поля формы в DOM
-	let nameInput = // Воспользуйтесь инструментом .querySelector()
-	let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-	// Получите значение полей из свойства value
-
-	// Выберите элементы, куда должны быть вставлены значения полей
-
-	// Вставьте новые значения с помощью textContent
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);*/ 
