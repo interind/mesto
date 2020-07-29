9a810dff09f37f63b588670624bf54af59e3f262
interind <interind@yandex.ru>
2020-07-29 21:51:59 +0300

    toggle

--- script/script.js
+++ script/script.js
@@ -1,21 +1,22 @@
 let popup = document.querySelector('.popup');
 let popupForm = document.querySelector('.popup__container');//форма
-let profileSection = document.querySelector('.profile');
-let profileInfo = profileSection.querySelector('.profile-info');
 let buttonOpenPopup = document.querySelector('.profile__edit-button');//кнопка открытия формы
 let buttonClosePopup = document.querySelector('.popup__button-close');//кнопка закрытия формы
-let buttonSubmit = popupForm.querySelector('.popup__button-submit');//кнопка отправки данных
 let profileTitle = document.querySelector('.profile__title');
-let popupInput = popupForm.querySelector('.popup__input');
 let nameInput = popupForm.querySelector('.popup__input_type_name');
 let jobInput = popupForm.querySelector('.popup__input_type_job');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
-function closePopup() {
-	popup.classList.remove('popup_opened');
+function poPup() {
+	popup.classList.toggle('popup_opened');
+	nameInput.value = profileTitle.textContent;
+	jobInput.value = profileSubtitle.textContent;
+	nameInput.focus();
 }
 
-buttonClosePopup.addEventListener('click' , closePopup);
+buttonOpenPopup.addEventListener('click', poPup);
+
+buttonClosePopup.addEventListener('click', poPup);
 
 function formSubmitHandler(evt) {
 	 evt.preventDefault(evt);
@@ -25,16 +26,7 @@ function formSubmitHandler(evt) {
 		}
 		profileTitle.textContent = nameInput.value;
 		profileSubtitle.textContent = jobInput.value;
-		closePopup();
-}
-
-function openPopup() {
-	popup.classList.add('popup_opened');
-	nameInput.value = profileTitle.textContent;
-	jobInput.value = profileSubtitle.textContent;
-	nameInput.focus();
+		poPup();
 }
 
 popupForm.addEventListener('submit', formSubmitHandler);
-
-buttonOpenPopup.addEventListener('click' , openPopup);
