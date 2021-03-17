c7bbf6d58789fb3bd757a8cc8295e805781f89f5
Aleksandr <interind@yandex.ru>
2021-03-17 17:06:33 +0300

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,59 @@
 const path = require('path'); // подключаем path к конфигу вебпак
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плаг
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин очистки папки при сборке
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');// научите «Вебпак» обрабатывать css-файлы
 
 module.exports = {
-    mode: 'development',
-    entry: { main: './src/pages/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-},
-module: {
- rules: [ // rules — это массив правил
-   // добавим в него объект правил для бабеля
-   {
-     // регулярное выражение, которое ищет все js файлы
-     test: /\.js$/,
-     // при обработке этих файлов нужно использовать babel-loader
-     loader: 'babel-loader',
-     // исключает папку node_modules, файлы в ней обрабатывать не нужно
-     exclude: '/node_modules/'
-   },
-   {
-    // регулярное выражение, которое ищет все файлы с такими расширениями
-    test: /\.(png|svg|jpg|gif)$/,
-    // при обработке этих файлов нужно использовать file-loader
-    loader: 'file-loader?name=./images/[name].[ext]'
+  entry: { main: './src/pages/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: ''
   },
-  {
-    // регулярное выражение, которое ищет все файлы с такими расширениями
-    test: /\.(eot|ttf|woff2|woff)$/,
-    // при обработке этих файлов нужно использовать file-loader
-    loader: 'file-loader?name=./vendor/[name].[ext]'
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+    open: true // сайт будет открываться сам при запуске npm run dev
   },
-  // аналогично добавьте правило для работы с html
-  {
-    test: /\.html$/,
-    loader: 'html-loader',
+  module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        loader: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      },
+      {// регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+      // при обработке этих файлов нужно использовать
+      // MiniCssExtractPlugin.loader и css-loader
+        use:  [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+        'postcss-loader'
+        ]
+      },
+    ]
   },
-  {
-   // применять это правило только к CSS-файлам
-     test: /\.css$/,
-   // при обработке этих файлов нужно использовать
-   // MiniCssExtractPlugin.loader и css-loader
-     loader:  [MiniCssExtractPlugin.loader, {
-       loader: 'css-loader',
-       // добавьте объект options
-       options: { importLoaders: 1 }
-     }, 'postcss-loader']
- }
-   ]
-},
-plugins: [new HtmlWebpackPlugin({
-template: './src/index.html',
-favicon: './src/images/russia.png'// путь к файлу index.html
-}),
-new MiniCssExtractPlugin(),// подключение плагина для объединения файлов
-] // добавьте массив
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/images/russia.png'// путь к файлу index.html
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin() // подключение плагина для объединения файлов
+  ]
 };
